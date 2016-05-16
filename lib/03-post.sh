734420562e053626182c241a1a9981fa3c4b08d5
Doug Thompson <service+git@dougthompson.co.uk>
2016-05-16 14:32:38 +0100

    fix: don't add to hostfile automatically

--- lib/03-post.sh
+++ lib/03-post.sh
@@ -8,5 +8,5 @@ log "npm & bower install"
 npm install
 bower install
 
-log "adding $1.dev to hostfile"
-sudo echo "127.0.0.1 $1.dev" >> /etc/hosts
+log "please add the following to your hostfile: 127.0.0.1 $1.dev"
+echo
