8358e76574971c00c7ff2ec5e955fd87429810ac
Doug Thompson <service+git@dougthompson.co.uk>
2017-02-28 13:53:59 +0000

    remove old template

--- template/front/src/scripts/components/nav-normal.jsx
+++
@@ -1,8 +0,0 @@
-let toggled = false;
-
-Util.ready(() => {
-  Util.findId('nav-toggle').onclick = () => {
-    toggled = !toggled;
-    Util.toggleClass(document.body, 'nav-toggled', toggled);
-  }
-});
--- template/front/src/scripts/main.jsx
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-require('../styles/main');
-require('expose?Util!./util');
-
-// require('./store'); // if you want redux
-// require('./components/nav');
